fac49fad2fd1e78760b311ae0356bb144a105e3f
Yash Mehan <yash.m@research.iiit.ac.in>
2021-10-26 14:24:38 +0530

    modified fork() so that child also gets mask member

--- kernel/proc.c
+++ kernel/proc.c
@@ -295,6 +295,8 @@ fork(void)
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
+  np-> mask = p-> mask; //copy the mask value
+  
   // increment reference counts on open file descriptors.
   for(i = 0; i < NOFILE; i++)
     if(p->ofile[i])

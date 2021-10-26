bd945b283180d6ab30391f55b58505c814e9d638
Yash Mehan <yash.m@research.iiit.ac.in>
2021-10-26 14:18:38 +0530

    created and wrote strace.c

---
+++ user/strace.c
@@ -0,0 +1,29 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char* argv[])
+{
+    char* command[MAXARG];
+    if(argc <= 2 || argv[1][0] == '-')
+    {
+        printf("incorrect usage of strace, exiting\n");
+        exit(1);
+    }
+    int mask_arg = atoi(argv[1]);
+    if(trace(mask_arg) <0)
+    {
+        printf("error in trace.c, requesting kernel to engage `trace` failed, exiting\n");
+        exit(1);
+    }
+
+    //if control reaches here that means inputs and args are fine, set up for exec
+    for(int i = 2; i< argc; i++)
+    {
+        command[i-2] = argv[i];
+    }
+    //real exec-ing
+    exec(command[0], command);
+    exit(0);
+}

24a0cc445450c5ec73ecb182b5ff87d6d0284d24
Yash Mehan <yash.m@research.iiit.ac.in>
2021-10-26 14:20:03 +0530

    added new member

--- kernel/proc.h
+++ kernel/proc.h
@@ -105,4 +105,5 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint mask;                   // added by yash for spec 1
 };
